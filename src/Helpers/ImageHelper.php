155ba8e65193b92bd19753848257768a6ca64967
ChristianGroeber <christian.groeber@bluewin.ch>
2021-01-30 21:53:43 +0100

    implemented image compression #3

---
+++ src/Helpers/ImageHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Nacho\Helpers;
+
+class ImageHelper
+{
+    public function compressImage(string $imagePath, int $size)
+    {
+        // Find out path of original image
+        $originalImgPath = '';
+        $splImgPath = explode('/', $imagePath);
+        $fileName = array_pop($splImgPath);
+        $originalImgPath = implode('/', $splImgPath);
+
+        // Scale down image
+        $imgObject = imagecreatefromstring(file_get_contents($imagePath));
+        $scaled = imagescale($imgObject, $size);
+
+        // Create new path if it does not exist yet
+        if (!is_dir("${originalImgPath}/${size}")) {
+            mkdir("${originalImgPath}/${size}", 0777, true);
+        }
+
+        // Save scaled down version in new path
+        imagejpeg($scaled, "${originalImgPath}/${size}/${fileName}");
+
+        return "${originalImgPath}/${size}/${fileName}";
+    }
+}
