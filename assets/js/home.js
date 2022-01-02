9f837fb7f365ecc0ec1d28c41c9ff4e70384c63e
ChristianGroeber <christian.groeber@bluewin.ch>
2022-01-02 13:11:23 +0100

    final build (?)

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,13 +4,8 @@ function fetchHomepage() {
         .then(response => response.json())
         .then(function (data) {
             data.forEach(function (month) {
-                let now = new Date();
-                let isFuture = false;
-                if (now.getMonth() < data.indexOf(month)) {
-                    isFuture = true;
-                }
                 console.log(month)
-                document.getElementById('months-list').innerHTML += '<div onclick="location.href=\'/' + month.name + '\'" style="background-image:url(\'' + month.image + '\')" class="month' + (isFuture ? " future" : "") + '"><p>' + month.name + '</p></div>';
+                document.getElementById('months-list').innerHTML += '<div onclick="location.href=\'/' + month.name + '\'" style="background-image:url(\'' + month.image + '\')" class="month"><p>' + month.name + '</p></div>';
             });
             removeLoadingIcon(document.querySelector('article'));
         });
